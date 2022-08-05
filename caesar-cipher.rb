dda77d09fd540b7a41eb147aac4ed38e077bc5db
Potayto <p.o.tayto@outlook.com>
2022-08-05 00:19:45 -0700

    Add logic for caesar cipher to work for lowercase strings

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,32 +1,48 @@
-=begin 
-  Define caesar_cipher(string, shift_factor, shift_direction)
-    - Split string into an array of elements, each one containing a character
-      from the string
-    - Use #map on the character array
-      - For each character in array, change it into its integer representation
-        by calling the #ord method on them
-      - Once the character has been converted into its integer representation,
-        use value of shift_direction to determine whether to add or subtract
-        the shift_factor from the integer representation of the character
-          - A value of "right" for the shift_direction means that the value of
-            shift_factor should be added to the value of the character's integer
-            representation
-          - On the other hand, a value of "left" means that the value should be
-            subtracted instead
-          - I need to determine what the values of "a", "A", "z", and "Z" are
-            and use them to wrap back around when the shift_factor goes beyond
-            either of those two boundaries
-              - If the new value goes over the ending boundary ("z" or "Z"),
-                the difference between the value and the ending boundary should
-                be added to the value of the beginning boundary in order to
-                correctly wrap back around the alphabet.
-              - The same should be done if the new value goes under the
-                beginning boundary ("a" or "A"), so that it can wrap around to
-                the end of the alphabet.
-      - Call the #chr method on the new value of the integer to convert it back
-        into a character
-      - The new array should now hold all of the new characters once the
-        shift_factor and shift_direction have been applied to them
-      - Lastly, I need to #join the array back into a string and return it
-        from the function
-=end
+def caesar_cipher(string, shift_factor, shift_direction)
+  character_array = string.split('')
+
+  modified_character_array = character_array.map do |element|
+    character_integer_value = element.ord
+
+    shifted_character_integer_value = calculate_new_value(character_integer_value,
+        shift_factor, shift_direction)
+
+    shifted_character_integer_value.chr
+  end
+
+  puts modified_character_array.join
+end
+
+def wrap_around_boundaries(character_integer_value)
+  # If character_integer_value goes over "z" boundary, wrap back around to "a"
+  # 1 is subtracted because wrapping back around counts as one step
+  if character_integer_value > 'z'.ord
+    ('a'.ord + (character_integer_value - 'z'.ord).abs) - 1
+  # If character_integer_value goes below "a" boundary, wrap back around to "z"
+  # 1 is added to account for the shifting that takes place by wrapping back around
+  elsif character_integer_value < 'a'.ord
+    ('z'.ord - (character_integer_value - 'a'.ord).abs) + 1
+  else
+    character_integer_value
+  end
+end
+
+def calculate_new_value(character_integer_value, shift_factor, shift_direction)
+  unless (character_integer_value >= 'A'.ord && character_integer_value <= 'Z'.ord) ||
+      (character_integer_value >= 'a'.ord && character_integer_value <= 'z'.ord)
+          return character_integer_value
+  end
+
+  case shift_direction
+  when "right"
+    character_integer_value += shift_factor
+  when "left"
+    character_integer_value -= shift_factor
+  else
+    character_integer_value
+  end
+
+  wrap_around_boundaries(character_integer_value)
+end
+
+caesar_cipher("hello world", 1, "left")
