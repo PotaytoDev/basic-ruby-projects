61514d83d85e097023e598784315294776ed04c6
Potayto <p.o.tayto@outlook.com>
2022-08-07 18:00:19 -0700

    Add logic for finding the best days to buy and sell stocks

--- stock-picker.rb
+++ stock-picker.rb
@@ -7,32 +7,41 @@
   => [1,4]  # for a profit of $15 - $3 == $12
 =end
 
-=begin
-  def stock_picker(stock_prices_array)
-    lowest_price = infinity
-    highest_price = 0
-    current_profit = 0
-    price_to_buy = infinity
-    price_to_sell = 0
-    final_profit = 0
+def stock_picker(stock_prices_array)
+  lowest_price = Float::INFINITY
+  highest_price = 0
+  current_profit = 0
+  price_to_buy = Float::INFINITY
+  price_to_sell = 0
+  final_profit = 0
 
-    - Ideally, the day to buy stocks would be the day with the lowest price.
-      - In order to find the lowest price, I can iterate through the array
-        and store in the variable lowest_price the lowest price I've come across.
-        - I need to compare the value of the current element to that which is
-          stored in the variable lowest_price.
-          - If current_value < lowest_price, lowest_price = current_value
-              highest_price = 0
-              profit = 0
-              continue
-          - If current_value > lowest_price:
-            - Compare current_value to that stored in highest_price
-              - If current_value > highest_price, highest_price = current_value
-              - Else continue to next value
-          - Calculate current_profit: highest_price - lowest_price
-            - If current_profit > final_profit
-                price_to_buy = lowest_price
-                price_to_sell = highest_price
-                final_profit = current_profit
-            - Else, continue
-=end
+  stock_prices_array.each do |stock_price|
+    if stock_price < lowest_price
+      lowest_price = stock_price 
+      highest_price = 0
+      profit = 0
+      next
+    elsif stock_price > lowest_price
+      if stock_price > highest_price
+        highest_price = stock_price
+      else
+        next
+      end
+    end
+
+    current_profit = highest_price - lowest_price
+
+    if current_profit > final_profit
+      price_to_buy = lowest_price
+      price_to_sell = highest_price
+      final_profit = current_profit
+    end
+  end
+
+  puts "Buy at $#{price_to_buy} on day #{stock_prices_array.index(price_to_buy) + 1}"
+  puts "Sell at $#{price_to_sell} on day #{stock_prices_array.index(price_to_sell) + 1}"
+  puts "For profit of $#{price_to_sell} - $#{price_to_buy} == $#{final_profit} per stock"
+  return [stock_prices_array.index(price_to_buy), stock_prices_array.index(price_to_sell)]
+end
+
+p stock_picker([17,3,6,9,15,8,6,1,10])
